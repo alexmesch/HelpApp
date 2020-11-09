e251a60ae6d1383bf948bb4563f5543b6f0b42a4
Alex Mesch <mesheryakov.alexander@mail.ru>
2020-11-09 20:21:56 +0300

    Splash screen done

---
+++ app/src/main/java/com/msch/helpapp/HelpActivity.kt
@@ -0,0 +1,11 @@
+package com.msch.helpapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HelpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
