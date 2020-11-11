853f6daf03d5cb49466ef2c31a083523f6c87b8e
Alex Mesch <mesheryakov.alexander@mail.ru>
2020-11-11 20:58:10 +0300

    Добавлен экран помощи. Реализован Splash Screen

---
+++ app/src/main/java/com/msch/helpapp/DataSource.kt
@@ -0,0 +1,27 @@
+package com.msch.helpapp
+
+import com.msch.helpapp.models.CategoryItem
+
+class DataSource {
+    companion object {
+        fun createDataSet() : ArrayList<CategoryItem> {
+            val list = ArrayList<CategoryItem>()
+            list.add(
+                CategoryItem("Дети",R.drawable.icon_kids)
+            )
+            list.add(
+                CategoryItem("Взрослые",R.drawable.icon_adults)
+            )
+            list.add(
+                CategoryItem("Пожилые",R.drawable.icon_elders)
+            )
+            list.add(
+                CategoryItem("Животные",R.drawable.icon_animals)
+            )
+            list.add(
+                CategoryItem("Мероприятия", R.drawable.icon_events)
+            )
+            return list
+        }
+    }
+}
---
+++ app/src/main/java/com/msch/helpapp/models/CategoryItem.kt
@@ -0,0 +1,7 @@
+package com.msch.helpapp.models
+
+data class CategoryItem (
+    var title: String,
+    var imageId : Int
+) {
+}
