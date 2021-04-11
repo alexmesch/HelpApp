6149b1cb55d8318206763cc2e566eddfebd605ca
Alex Mesch <mesheryakov.alexander@mail.ru>
2021-04-11 21:00:15 +0300

    Улучшил функцию retrieveEventsData(). Использовал map и лямбду. Понравилось

--- app/src/main/java/com/msch/helpapp/database/FirebaseOperations.kt
+++ app/src/main/java/com/msch/helpapp/database/FirebaseOperations.kt
@@ -1,7 +1,6 @@
 package com.msch.helpapp.database
 
 import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.GenericTypeIndicator
 import com.msch.helpapp.models.CategoryItems
 import com.msch.helpapp.models.EventDetails
 
@@ -17,30 +16,30 @@ object FirebaseOperations {
         return result
     }
 
-    fun retrieveEventsData(dataSnapshot: DataSnapshot, firebaseChild: String): ArrayList<EventDetails>{
-        val listFormat: GenericTypeIndicator<List<String>> = object: GenericTypeIndicator<List<String>>(){}
-        var result: ArrayList<EventDetails> = ArrayList()
-        val errorImageList: List<String> = listOf("cardimage")
-        lateinit var item: EventDetails
+    fun retrieveEventsData(dataSnapshot: DataSnapshot, firebaseChild: String): List<EventDetails>{
+        val errorList: List<EventDetails> = listOf(EventDetails())
 
-        for (ds in dataSnapshot.child(firebaseChild).children) {
-            item = EventDetails(
-                ds.child("eventId").value.toString(),
-                ds.child("eventCategory").value.toString(),
-                ds.child("eventMainImage").value.toString(),
-                ds.child("eventSecondaryImages").getValue(listFormat)?: errorImageList,
-                ds.child("eventFriends").getValue(listFormat)?: errorImageList,
-                ds.child("eventName").value.toString(),
-                ds.child("eventDate").value.toString(),
-                ds.child("eventOrganizer").value.toString(),
-                ds.child("eventLocation").value.toString(),
-                ds.child("eventContacts").value.toString(),
-                ds.child("eventMail").value.toString(),
-                ds.child("eventDescription").value.toString(),
-                ds.child("eventSite").value.toString()
-            )
-            result.add(item)
-        }
-        return result
+        val fbResponse = dataSnapshot.child(firebaseChild).children
+        return fbResponse.map { it.getValue(EventDetails::class.java) ?: errorList } as List<EventDetails>
+
+        /*dataSnapshot.child(firebaseChild).children.forEach {
+            item = it.getValue(EventDetails::class.java)!!
+            /*item = EventDetails(
+                it.child("eventId").value.toString(),
+                it.child("eventCategory").value.toString(),
+                it.child("eventMainImage").value.toString(),
+                it.child("eventSecondaryImages").getValue(listFormat)?: errorImageList,
+                it.child("eventFriends").getValue(listFormat)?: errorImageList,
+                it.child("eventName").value.toString(),
+                it.child("eventDate").value.toString(),
+                it.child("eventOrganizer").value.toString(),
+                it.child("eventLocation").value.toString(),
+                it.child("eventContacts").value.toString(),
+                it.child("eventMail").value.toString(),
+                it.child("eventDescription").value.toString(),
+                it.child("eventSite").value.toString()
+            )*/
+            //result.add(item)
+        }*/
     }
 }
